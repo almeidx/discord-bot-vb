5d10b57d017096b5f790f8b38e8d0ce69e440d94
almeidx <almeidx@pm.me>
2020-10-31 14:42:12 +0000

    style: use consistent indentation

--- DiscordBot/Module1.vb
+++ DiscordBot/Module1.vb
@@ -3,79 +3,81 @@ Imports Discord
 Imports Discord.WebSocket
 
 Module Module1
-	Private _client as DiscordSocketClient
-	Private ReadOnly UserActions as Dictionary(of String, Action(Of SocketMessage, DiscordSocketClient)) = New Dictionary(Of String,Action(Of SocketMessage,DiscordSocketClient))()
-    Sub Main()
-	    DotNetEnv.Env.Load()
-	    UserActions.Add("!ping", AddressOf Pong)
-	    UserActions.Add("!help", AddressOf Help)
-        MainAsync().GetAwaiter().GetResult()
-    End Sub
+  Private _client as DiscordSocketClient
+  Private ReadOnly Commands As Dictionary(Of String, Action(Of SocketMessage, DiscordSocketClient)) = New Dictionary(Of String, Action(Of SocketMessage, DiscordSocketClient))()
 
-    Private Async Function MainAsync() As Task
-	    Dim config As New DiscordSocketConfig With {
-			.LogLevel = LogSeverity.Info
-		}
+  Sub Main()
+    DotNetEnv.Env.Load()
+    Commands.Add("!ping", AddressOf PongCommand)
+    Commands.Add("!help", AddressOf HelpCommand)
+    MainAsync().GetAwaiter().GetResult()
+  End Sub
 
-		_client = new DiscordSocketClient(config)
+  Private Async Function MainAsync() As Task
+    Dim config As New DiscordSocketConfig With {
+      .LogLevel = LogSeverity.Info
+    }
 
-	    AddHandler _client.Log, AddressOf Log
+    _client = new DiscordSocketClient(config)
 
-		Await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"))
-		Await _client.StartAsync()
+    AddHandler _client.Log, AddressOf Log
 
-		AddHandler _client.Ready, AddressOf Ready
-	    AddHandler _client.MessageReceived, AddressOf MessageCreated
-	    AddHandler _client.MessageDeleted, AddressOf MessageDel
-		Await Task.Delay(-1)
-    End Function
+    Await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"))
+    Await _client.StartAsync()
 
-	Private Function Log(msg as LogMessage) as Task
-		Console.WriteLine(msg)
-		return Task.CompletedTask 
-	End Function
+    AddHandler _client.Ready, AddressOf Ready
+    AddHandler _client.MessageReceived, AddressOf MessageCreated
+    AddHandler _client.MessageDeleted, AddressOf MessageDel
+    Await Task.Delay(-1)
+  End Function
 
-	Private Async Function Ready() as Task
-		Await _client.SetGameAsync("my dev suffer from using VB", type := ActivityType.Watching)
-	End Function
+  Private Function Log(msg as LogMessage) as Task
+    Console.WriteLine(msg)
+    return Task.CompletedTask
+  End Function
 
-	Private Function MessageCreated(message as SocketMessage) as Task
-		Console.WriteLine($"[{Date.Now()}] -> {message.Author.Username}#{message.Author.Discriminator}: {message.Content}")
+  Private Async Function Ready() as Task
+    Await _client.SetGameAsync("my dev suffer from using VB", type := ActivityType.Watching)
+  End Function
 
-		If UserActions.ContainsKey(message.Content)
-			UserActions(message.Content).Invoke(message, _client)
-		End If
+  Private Function MessageCreated(message as SocketMessage) as Task
+    Console.WriteLine($"[{Date.Now()}] -> {message.Author.Username}#{message.Author.Discriminator}: {message.Content}")
 
-		return Task.CompletedTask
-	End Function
+    Dim command As String = message.Content.Split(" ")(0)
+    If Commands.ContainsKey(command)
+      Commands(command).Invoke(message, _client)
+    End If
 
-	Private Function MessageDel(message As Cacheable(Of IMessage, UInt64), channel As ISocketMessageChannel) as Task
-		If message.HasValue
-			Dim msg = message.Value
-			Console.WriteLine($"Message from {msg.Author.Username}#{msg.Author.Discriminator} deleted: {msg.Content}")
-		End If
+    return Task.CompletedTask
+  End Function
 
-		return Task.CompletedTask
-	End Function
-	
-	Private Function GenerateEmbed(author As IUser, Optional description As String = Nothing)
-		Dim builder = new EmbedBuilder()
-		If description <> Nothing And description <> ""
-			builder.WithDescription(description)
-		End If
-		builder.WithFooter($"{author.Username}#{author.Discriminator}")
-		builder.WithAuthor($"{author.Username}#{author.Discriminator}", author.GetAvatarUrl())
-		builder.WithTimestamp(Date.Now())
-		builder.WithColor(Color.Blue)
-		return builder.Build()
-	End Function
-	
-	Private Sub Pong(m As SocketMessage, c As DiscordSocketClient)
-		m.Channel.SendMessageAsync($"Pong! {c.Latency}ms") 
-	End Sub
-	
-	Private Sub Help(m As SocketMessage, c As DiscordSocketClient)
-		Dim embed As Embed = GenerateEmbed(m.Author, "E que fags")
-		m.Channel.SendMessageAsync(Nothing, False, embed)
-	End Sub
+  Private Function MessageDel(message As Cacheable(Of IMessage, UInt64), channel As ISocketMessageChannel) as Task
+    If message.HasValue
+      Dim msg = message.Value
+      Console.WriteLine($"Message from {msg.Author.Username}#{msg.Author.Discriminator} deleted: {msg.Content}")
+    End If
+
+    return Task.CompletedTask
+  End Function
+
+  Private Function GenerateEmbed(author As IUser, Optional description As String = Nothing)
+    Dim builder = new EmbedBuilder()
+    If description <> Nothing And description <> ""
+      builder.WithDescription(description)
+    End If
+    builder.WithFooter($"{author.Username}#{author.Discriminator}")
+    builder.WithAuthor($"{author.Username}#{author.Discriminator}", author.GetAvatarUrl())
+    builder.WithTimestamp(Date.Now())
+    builder.WithColor(Color.Blue)
+    return builder.Build()
+  End Function
+
+  Private Sub PongCommand(m As SocketMessage, c As DiscordSocketClient)
+    m.Channel.SendMessageAsync($"Pong! {c.Latency}ms")
+  End Sub
+
+  Private Sub HelpCommand(m As SocketMessage, c As DiscordSocketClient)
+    Dim embed As Embed = GenerateEmbed(m.Author, "E que fags")
+    m.Channel.SendMessageAsync(Nothing, False, embed)
+  End Sub
 End Module
